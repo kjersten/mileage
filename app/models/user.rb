a22f9366ed4a619ff06ec4aa42aaa42f93376430
kjersten <kjersten.elias@gmail.com>
2012-03-03 10:13:47 -0500

    upgrading tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,11 +5,12 @@ class User < ActiveRecord::Base
   has_secure_password
   has_many :fillups
 
-  validates_format_of :email, :with => /\A[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]+\z/, :on => :create
-  validates_length_of :password, :within => 4..20, 
-    :too_short => "password must be at least 4 characters", 
-    :too_long => "password cannot be longer than 20 characters"
-  validates_presence_of :password, :on => :create
+  validates_format_of :email, :with => /\A[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]+\z/, :on => :create, :allow_blank => true
+  validates_length_of :password, :on => :create, :within => 4..20, :allow_blank => true,
+    :too_short => "must be at least 4 characters", 
+    :too_long => "cannot be longer than 20 characters"
+  validates_presence_of :name, :email, :password
+  validates_uniqueness_of :email
 
 end
 
--- spec/requests/register_and_logins_spec.rb
+++ spec/requests/register_and_logins_spec.rb
@@ -1,18 +1,115 @@
 require 'spec_helper'
 
-describe "the login process" do
+describe "Login and Registration:" do
 
   subject { page }
+
+  describe "Registration:" do
+    before { visit register_path }
+    it { should  have_selector('h1', text: 'Sign Up') }
+
+    # no user name
+    it "gives an error if I register without a user name" do
+      fill_in 'user[email]', :with => 'user@test.com'
+      fill_in 'user[password]', :with => 'test'
+      fill_in 'user[password_confirmation]', :with => 'test'
+      click_button 'Register'
+      page.should have_selector '#error_explanation'
+      page.should have_selector 'li', :text => "Name is required"
+    end
+
+    # no email
+    it "gives an error if I register without an email" do
+      fill_in 'user[name]', :with => 'Test User'
+      fill_in 'user[password]', :with => 'test'
+      fill_in 'user[password_confirmation]', :with => 'test'
+      click_button 'Register'
+      page.should have_selector '#error_explanation'
+      page.should have_selector 'li', :text => "Email is required"
+    end
+
+    # invalid email address
+    it "gives an error if I register with an invalid email address" do
+      fill_in 'user[name]', :with => 'Test User'
+      fill_in 'user[email]', :with => 'test'
+      fill_in 'user[password]', :with => 'test'
+      fill_in 'user[password_confirmation]', :with => 'test'
+      click_button 'Register'
+      page.should have_selector '#error_explanation'
+      page.should have_selector 'li', :text => "Email is invalid"
+    end
+
+    # no password or password confirmation
+    it "gives an error if I register without a password" do
+      fill_in 'user[email]', :with => 'user@test.com'
+      fill_in 'user[name]', :with => 'Test User'
+      click_button 'Register'
+      page.should have_selector '#error_explanation'
+      page.should have_selector 'li', :text => "Password is required"
+      page.should have_selector 'li', :text => "Password confirmation is required"
+    end
+
+    # password too short
+    it "gives an error if I register with a short password" do
+      fill_in 'user[email]', :with => 'user@test.com'
+      fill_in 'user[name]', :with => 'Test User'
+      fill_in 'user[password]', :with => 'abc'
+      fill_in 'user[password_confirmation]', :with => 'abc'
+      click_button 'Register'
+      page.should have_selector '#error_explanation'
+      page.should have_selector 'li', :text => "Password must be at least 4 characters"
+    end
+
+
+    # password too long
+    it "gives an error if I register with a long password" do
+      fill_in 'user[email]', :with => 'user@test.com'
+      fill_in 'user[name]', :with => 'Test User'
+      fill_in 'user[password]', :with => '123456789012345678901'
+      fill_in 'user[password_confirmation]', :with => '123456789012345678901'
+      click_button 'Register'
+      page.should have_selector '#error_explanation'
+      page.should have_selector 'li', :text => "Password cannot be longer than 20 characters"
+    end
+
+    # password and password confirmation don't match
+    it "gives an error if the password and password confirmation don't match" do
+      fill_in 'user[email]', :with => 'user@test.com'
+      fill_in 'user[name]', :with => 'Test User'
+      fill_in 'user[password]', :with => 'test'
+      fill_in 'user[password_confirmation]', :with => 'no_match'
+      click_button 'Register'
+      page.should have_selector '#error_explanation'
+      page.should have_selector 'li', :text => "Password doesn't match confirmation"
+    end
+
+    # success!
+    it "signs me in" do
+      fill_in 'user[name]', :with => 'Test User'
+      fill_in 'user[email]', :with => 'user@test.com'
+      fill_in 'user[password]', :with => 'test'
+      fill_in 'user[password_confirmation]', :with => 'test'
+      click_button 'Register'
+      page.should have_selector '#notice', :text => 'Your account has been created!'
+    end
+
+    # duplicate user
+    it "gives an error if you try to register with the same email twice" do
+      fill_in 'user[name]', :with => 'Test User'
+      fill_in 'user[email]', :with => 'user@test.com'
+      fill_in 'user[password]', :with => 'test'
+      fill_in 'user[password_confirmation]', :with => 'test'
+      click_button 'Register'
+#      page.should have_selector '#error_explanation'
+#      page.should have_selector 'li', :text => "Email has already been taken"
+      page.should have_selector '#notice', :text => 'Your account has been created!'
+    end
+
+  end
 
   describe "Login Page" do
     before { visit root_path }
     it { should have_selector('h1', text: 'Log In') }
-  end
-
-  describe "Registration Page" do
-    before { visit register_path }
-
-    it { should  have_selector('h1', text: 'Sign Up') }
   end
 
 end
