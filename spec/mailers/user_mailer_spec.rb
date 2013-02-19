3b1b75b544a60ffbcc657f9e7d2a7f3eda1b5e67
kjersten <kjersten.elias@gmail.com>
2013-02-18 16:08:59 -0800

    updating UserMailer test to check for correct values

--- spec/mailers/user_mailer_spec.rb
+++ spec/mailers/user_mailer_spec.rb
@@ -2,12 +2,13 @@ require "spec_helper"
 
 describe UserMailer do
   describe "password_reset" do
-    let(:mail) { UserMailer.password_reset }
+    let(:user) {FactoryGirl.create(:user)}
+    let(:mail) {UserMailer.password_reset(user)}
 
     it "renders the headers" do
-      mail.subject.should eq("Password reset")
-      mail.to.should eq(["to@example.org"])
-      mail.from.should eq(["from@example.com"])
+      mail.subject.should eq("Password Reset")
+      mail.to.should eq([user.email])
+      mail.from.should eq(["no_reply@mileage_tracker.com"])
     end
 
     it "renders the body" do
