ebd4f9d70b4d947978efaad238e213d50b58528c
kjersten <kjersten.elias@gmail.com>
2013-02-18 18:23:41 -0800

    correcting the test for the password_resets controller

--- spec/controllers/password_resets_controller_spec.rb
+++ spec/controllers/password_resets_controller_spec.rb
@@ -4,11 +4,19 @@ describe PasswordResetsController do
 
   let(:user) {FactoryGirl.create(:user)}
 
-  describe "#create password reset token" do
-    it "should " do
-      user.stub(:send_password_reset) {}
-      post 'create', :email => user.email
-      user.should_receive(:send_password_reset)
+  describe "POST create" do
+    before(:each) do
+      post :create, :email => user.email
+    end
+
+    it "should set a new password_reset_token" do
+      upd_usr = User.find_by_email user.email
+      upd_usr.password_reset_token.should_not be_nil
+    end
+
+    it "should redirect with a notice" do
+      response.should redirect_to(login_path)
+      flash[:notice].should_not be_nil
     end
   end
 
